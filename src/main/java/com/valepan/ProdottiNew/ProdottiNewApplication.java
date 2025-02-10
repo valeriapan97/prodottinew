682675828890d4dc4ce3e6a62ef8c7260519bfec
Pannone <valeria.pannone@accenture.com>
2025-02-10 15:59:48 +0100

    Modifica il file .gitignore per lasciarlo vuoto

---
+++ src/main/java/com/valepan/ProdottiNew/ProdottiNewApplication.java
@@ -0,0 +1,14 @@
+package com.valepan.ProdottiNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdottiNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdottiNewApplication.class, args);
+		System.out.println("Buongiorno");
+	}
+
+}
